e81359f091b365c8a81347717c3dcb9864c9a2d6
Jari Duyvejonck <jari.duyvejonck@outlook.com>
2020-12-30 19:32:28 +0100

    init project

---
+++ src/main/java/jari/duyvejonck/SunnyPortaltoDB/SunnyPortalToDbApplication.java
@@ -0,0 +1,13 @@
+package jari.duyvejonck.SunnyPortaltoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyPortalToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnyPortalToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jari/duyvejonck/SunnyPortaltoDB/SunnyPortalToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package jari.duyvejonck.SunnyPortaltoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnyPortalToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
